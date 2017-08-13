7d3c9c0cc6c67f688b3952b4efa489730d63c233
Jessica Gertler <jessicadgertler@gmail.com>
2017-08-13 16:27:06 -0600

    First Commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+
+    $('.material-icons').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) {console.log("onOpen working")}, 
+      onClose: function(el) {console.log("onCloseworking")},
+ });
+}); //end doc ready
+          
