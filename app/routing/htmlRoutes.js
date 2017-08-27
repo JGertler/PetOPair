b2ec079b11b37ede8d591e6b46abed8eac4ffa94
Mayat Smolnik <mcsmolnik@gmail.com>
2017-08-27 15:25:36 -0600

    some auth

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,29 +1,73 @@
-var path= require("path");
+var path = require("path");
+
+
+
 
 var db = require("../models");
 
-module.exports = function (app) {
+var authredirect = require("../routing/apiRoutes.js");
 
-	app.get("/", function(request, response){
-		response.sendFile(path.join(__dirname + '/../../views/index.html'));
-	});
-	app.get("/signup", function(request, response){
-		response.sendFile(path.join(__dirname + '/../../views/signUp.html'));
-	});
-	app.get("/aboutme", function(request, response){
-		response.sendFile(path.join(__dirname + '/../../views/aboutMe.html'));
-	});
+module.exports = function(app, passport) {
 
-	app.get("/sugar", function(request, response){
-		response.sendFile(path.join(__dirname + '/../../views/sugar.html'));
-	});
+  app.get("/", function(request, response) {
+    response.sendFile(path.join(__dirname + '/../../views/index.html'));
+  });
+  app.get("/signup", function(request, response) {
+    response.sendFile(path.join(__dirname + '/../../views/signUp.html'));
+  });
+  app.get("/aboutme", function(request, response) {
+    response.sendFile(path.join(__dirname + '/../../views/aboutMe.html'));
+  });
 
-
-	app.get("/bulletin", function(request, response){
-		response.sendFile(path.join(__dirname + '/../../views/bulletin.html'));
-	});
+  app.get("/sugar", function(request, response) {
+    response.sendFile(path.join(__dirname + '/../../views/sugar.html'));
+  });
 
 
-//path to post human info data to my terminal
+  app.get("/bulletin", isLoggedIn, function(request, response) {
+    response.sendFile(path.join(__dirname + '/../../views/bulletin.html'));
+  });
+
+
+
+  //app.get('/dashboard',isLoggedIn, authController.dashboard);
+
+
+  app.get('/logout', exports.logout = function(req, res) {
+    req.logOut();
+    //req.session.destroy(function(err) {
+    res.redirect('/');
+  });
+  //});
+
+  // app.post('/put_newuser_in_db', passport.authenticate('local-signup',  { successRedirect: '/bulletin', failureRedirect: '/signup'}));
+
+  app.post('/put_newuser_in_db',
+    passport.authenticate('local-signup'),
+
+    function(req, res) {
+      // If this function gets called, authentication was successful.
+      // `req.user` contains the authenticated user.
+      res.json(req.user);
+    });
+
+  app.post('/signin',
+    passport.authenticate('local-signin'),
+    function(req, res) {
+      // If this function gets called, authentication was successful.
+      // `req.user` contains the authenticated user.
+      res.json(req.user);
+    });
+
+
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+			console.log(req);
+      return next();
+    }
+
+    res.redirect('/');
+  }
+
 
 }
