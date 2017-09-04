8857f01d2a57a6d7044f197275b9d628c5dcdcaf
Jessica Gertler <jessicadgertler@gmail.com>
2017-09-04 16:09:27 -0600

    .gitignore now working

--- app/config/keys.js
+++
@@ -1,9 +0,0 @@
-var keys = {
-	"mapKey":"AIzaSyBaw-4l7qS4b_L7kXhuHViE2smEu1k34Dw",
-	"sqlKey": "yDNUdYkTmOhD",
-	"s3bucket": "petopair-west",
-	"s3accesskey": "AKIAJMLQALSQKRWKG3VA",
-	"s3secretaccesskey": "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",
-};
-
-module.exports = keys;
